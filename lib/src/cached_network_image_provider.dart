d42bee3c1c3af30682ab8c7fd74a7744c91cafcb
Hugo Passos <hugocbpassos@gmail.com>
2019-12-11 16:34:17 -0300

    Make URLs mockable

--- lib/src/cached_network_image_provider.dart
+++ lib/src/cached_network_image_provider.dart
@@ -1,5 +1,4 @@
 import 'dart:async' show Future;
-import 'dart:io' show File;
 import 'dart:typed_data';
 import 'dart:ui' as ui show instantiateImageCodec, Codec;
 
@@ -9,8 +8,7 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 typedef void ErrorListener();
 
-class CachedNetworkImageProvider
-    extends ImageProvider<CachedNetworkImageProvider> {
+class CachedNetworkImageProvider extends ImageProvider<CachedNetworkImageProvider> {
   /// Creates an ImageProvider which loads an image from the [url], using the [scale].
   /// When the image fails to load [errorListener] is called.
   const CachedNetworkImageProvider(this.url,
@@ -32,15 +30,35 @@ class CachedNetworkImageProvider
   // Set headers for the image provider, for example for authentication
   final Map<String, String> headers;
 
+  /// Mock URLs and its respective bytes.
+  static final Map<String, Uint8List> _mockUrls = {};
+
+  /// Sets a mock URL.
+  ///
+  /// When loading an image from the [url], instead of relying on [cacheManager]
+  /// to download and cache the file, your [bytes] are going to be used.
+  ///
+  /// Since there won't be any HTTP request and the SQLite database won't be
+  /// reached, this mock can be used to avoid platform-channel interactions.
+  ///
+  /// Accordingly, the caching features of this library won't apply to the
+  /// [url], once nothing is going to be cached after all.
+  static void setMockUrl(String url, Uint8List bytes) {
+    assert(url != null && url.isNotEmpty);
+    assert(bytes != null && bytes.lengthInBytes > 0);
+    _mockUrls[url] = bytes;
+  }
+
+  /// Clears the mock URLs set through [setMockUrl].
+  static void clearMockUrls() => _mockUrls.clear();
+
   @override
-  Future<CachedNetworkImageProvider> obtainKey(
-      ImageConfiguration configuration) {
+  Future<CachedNetworkImageProvider> obtainKey(ImageConfiguration configuration) {
     return SynchronousFuture<CachedNetworkImageProvider>(this);
   }
 
   @override
-  ImageStreamCompleter load(
-      CachedNetworkImageProvider key, DecoderCallback decode) {
+  ImageStreamCompleter load(CachedNetworkImageProvider key, DecoderCallback decode) {
     return MultiFrameImageStreamCompleter(
       codec: _loadAsync(key),
       scale: key.scale,
@@ -55,27 +73,32 @@ class CachedNetworkImageProvider
   }
 
   Future<ui.Codec> _loadAsync(CachedNetworkImageProvider key) async {
-    var mngr = cacheManager ?? DefaultCacheManager();
-    var file = await mngr.getSingleFile(url, headers: headers);
-    if (file == null) {
-      if (errorListener != null) errorListener();
-      return Future<ui.Codec>.error("Couldn't download or retrieve file.");
+    Uint8List bytes;
+
+    if (_mockUrls.containsKey(url)) {
+      bytes = _mockUrls[url];
+    } else {
+      bytes = await _loadBytesWithCacheManager();
     }
-    return await _loadAsyncFromFile(key, file);
-  }
-
-  Future<ui.Codec> _loadAsyncFromFile(
-      CachedNetworkImageProvider key, File file) async {
-    assert(key == this);
-
-    final Uint8List bytes = await file.readAsBytes();
 
     if (bytes.lengthInBytes == 0) {
       if (errorListener != null) errorListener();
-      throw Exception("File was empty");
+      throw new Exception("File was empty");
     }
 
     return await ui.instantiateImageCodec(bytes);
+  }
+
+  Future<Uint8List> _loadBytesWithCacheManager() async {
+    var mngr = cacheManager ?? DefaultCacheManager();
+    var file = await mngr.getSingleFile(url, headers: headers);
+
+    if (file == null) {
+      if (errorListener != null) errorListener();
+      return Future<Uint8List>.error("Couldn't download or retrieve file.");
+    }
+
+    return await file.readAsBytes();
   }
 
   @override
